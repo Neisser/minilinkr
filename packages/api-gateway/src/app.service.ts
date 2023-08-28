115e8658d0ddac042956ef4568d95627c73b085f
Neisser <villa.neisser@gmail.com>
2023-08-27 22:00:51 -0500

    feat: added @minilinkr/url to the api-gateway

--- packages/api-gateway/src/app.service.ts
+++ packages/api-gateway/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { getUrlMessage } from '@minilinkr/urls';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log('hello')
+    return getUrlMessage();
   }
 }
