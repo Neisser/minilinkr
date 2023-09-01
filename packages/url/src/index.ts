f93cd022fe472bcb8328eb53ee2c48f25b03546f
Neisser <villa.neisser@gmail.com>
2023-08-31 23:48:40 -0500

    feat(url): mock url use cases for getting and generating urls

--- packages/url/src/index.ts
+++ packages/url/src/index.ts
@@ -1,3 +1,14 @@
-export function getUrlMessage() {
-  return 'Hello from url';
+import { toBase62 } from './toBase62';
+
+export function getUrlMessage(urlHash: string): string {
+  const dummyUrls = {
+    '5I1Ayh1': 'https://github.com/Neisser/neisser',
+    'glql81': 'https://docs.nestjs.com/controllers#request-payloads'
+  }
+  return dummyUrls[urlHash];
+}
+
+export function generateHash(id: number): string {
+  const hashUrl = toBase62(id);
+  return hashUrl;
 }
